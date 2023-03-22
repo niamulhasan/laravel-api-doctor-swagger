cc45d47e13dec000d92ded003cc8709de3b569a7
Niamul Hasan <niamulhasanbd@gmail.com>
2023-03-22 17:03:53 +0600

    Update index.blade.php

--- src/resources/views/index.blade.php
+++ src/resources/views/index.blade.php
@@ -1,12 +1,12 @@
 <html>
 <head>
     <title>{{ config('app.name') }} | Frontend API's Swagger</title>
-    <link href="{{asset('swagger/style.css')}}" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/swagger-ui-dist@3.17.0/swagger-ui.css" rel="stylesheet">
 </head>
 <body>
 <div id="swagger-ui"></div>
-<script src="{{asset('swagger/jquery-2.1.4.min.js')}}"></script>
-<script src="{{asset('swagger/swagger-bundle.js')}}"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+<script src="https://unpkg.com/swagger-ui-dist@3.23.1/swagger-ui-bundle.js"></script>
 <script type="application/javascript">
     const ui = SwaggerUIBundle({
         url: "{{ asset(URL::to('/').'/swagger-yaml-file-generated') }}",
